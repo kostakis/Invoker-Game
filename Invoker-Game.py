63b8489a5f4826659000c2cc699f8f49e2219ac9
yourdeathgg <75530853+DerZelenevskiy@users.noreply.github.com>
2022-04-20 11:10:22 +0700

    Add files via upload
    Base file added

--- Invoker-Game.py
+++ Invoker-Game.py
@@ -5,10 +5,24 @@ height = 1080
 fps = 60
 
 pygame.init()
+global screen
 screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 
+class GameSprite(Sprite):
+        def __init__(self, image, x, y):
+                self.image = pygame.image.load(image)
+                self.rect = image.get_rect()
+                self.rect.x = x
+                self.rect.y = y
+
+        def blit(self):
+                screen.blit(self.image, (self.rect.x, self.rect.y))
+
+class Icon(GameSprite)
+        
 while True:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                         quit()
+        pygame.display.update()
